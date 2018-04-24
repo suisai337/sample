17378aad4ef11d586b659851e06f624c6190effa
Yan Fan <330483691@qq.com>
2018-04-24 09:10:04 +0000

    Finish login and logout

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,9 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-Route::get('singup','UserController@create')->name('singup');
+Route::get('signup','UserController@create')->name('signup');
 Route::resource('users','UserController');
+
+Route::get('login','SessionsController@create')->name('login');
+Route::post('login','SessionsController@store')->name('login');
+Route::delete('logout','SessionsController@destroy')->name('logout');
